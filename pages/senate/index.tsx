5652a45e43c02e366d06513f66c2e98f0dafa158
Linkai Wu <linkai@linkaiwu.com>
2022-10-19 11:01:17 -0400

    Update headers and titles for SEO

--- pages/senate/index.tsx
+++ pages/senate/index.tsx
@@ -25,7 +25,7 @@ export default function SenatePage({ latestDate, candidates, incumbents, average
       <section className="p-8 container max-w-4xl flex flex-col gap-4 bg-neutral-50 border-2 shadow-md rounded-2xl">
         <div className="flex flex-col gap-1.5 items-center">
           <h1 className="px-1.5 text-xl text-center font-medium uppercase bg-amber-100 rounded-md">
-            The Senate
+             Senate Forecast
           </h1>
           <p className="text-4xl text-center font-serif">
             {demWinChance>0.7 ?
