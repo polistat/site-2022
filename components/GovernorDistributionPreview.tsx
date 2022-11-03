85e93c37d9833033985ac4f1ad13823da96f75ea
Linkai Wu <linkai@linkaiwu.com>
2022-11-03 14:40:13 -0400

    Fix governor dist charts

--- components/GovernorDistributionPreview.tsx
+++ components/GovernorDistributionPreview.tsx
@@ -3,15 +3,16 @@ import React from 'react';
 export default function GovernorDistributionPreview({ incumbents, averagedPolls }: { incumbents: any, averagedPolls: any }) {
   const dist = [
     Object.keys(incumbents.governor).filter((a:any) => incumbents.governor[a][0].party === 'democrat'),
-    averagedPolls.filter((a:any) => { return a.office==='Governor' && a.lean/100>0.7 }).map((a:any) => a.state_po),
-    averagedPolls.filter((a:any) => { return a.office==='Governor' && a.lean/100>0.6 && a.lean/100<=0.7 }).map((a:any) => a.state_po),
-    averagedPolls.filter((a:any) => { return a.office==='Governor' && a.lean/100>0.55 && a.lean/100<=0.6 }).map((a:any) => a.state_po),
-    averagedPolls.filter((a:any) => { return a.office==='Governor' && a.lean/100>0.45 && a.lean/100<=0.55 }).map((a:any) => a.state_po),
-    averagedPolls.filter((a:any) => { return a.office==='Governor' && a.lean/100>0.4 && a.lean/100<=0.45 }).map((a:any) => a.state_po),
-    averagedPolls.filter((a:any) => { return a.office==='Governor' && a.lean/100>0.3 && a.lean/100<=0.4 }).map((a:any) => a.state_po),
-    averagedPolls.filter((a:any) => { return a.office==='Governor' && a.lean/100<=0.3 }).map((a:any) => a.state_po),
+    averagedPolls.filter((a:any) => { return a.office==='Governor' && a.dem_wins/100>0.7 }).map((a:any) => a.state_po),
+    averagedPolls.filter((a:any) => { return a.office==='Governor' && a.dem_wins/100>0.6 && a.dem_wins/100<=0.7 }).map((a:any) => a.state_po),
+    averagedPolls.filter((a:any) => { return a.office==='Governor' && a.dem_wins/100>0.55 && a.dem_wins/100<=0.6 }).map((a:any) => a.state_po),
+    averagedPolls.filter((a:any) => { return a.office==='Governor' && a.dem_wins/100>0.45 && a.dem_wins/100<=0.55 }).map((a:any) => a.state_po),
+    averagedPolls.filter((a:any) => { return a.office==='Governor' && a.dem_wins/100>0.4 && a.dem_wins/100<=0.45 }).map((a:any) => a.state_po),
+    averagedPolls.filter((a:any) => { return a.office==='Governor' && a.dem_wins/100>0.3 && a.dem_wins/100<=0.4 }).map((a:any) => a.state_po),
+    averagedPolls.filter((a:any) => { return a.office==='Governor' && a.dem_wins/100<=0.3 }).map((a:any) => a.state_po),
     Object.keys(incumbents.governor).filter((a:any) => incumbents.governor[a][0].party === 'republican'),
   ];
+
 
   const distStyles = [
     'stroke-[3px] stroke-blue-400 fill-neutral-50',
@@ -33,7 +34,7 @@ export default function GovernorDistributionPreview({ incumbents, averagedPolls 
   return <>
     <svg
       strokeLinejoin="round"
-      viewBox="0 0 1000 202"
+      viewBox="0 0 1000 252"
     >
 
       {dist.map((a,i) => <>
@@ -42,49 +43,49 @@ export default function GovernorDistributionPreview({ incumbents, averagedPolls 
           x={5+110*i}
           y={0}
           width={110}
-          height={180}
+          height={230}
           className={`fill-transparent`}
         />
 
         {/* baseline */}
         <path
           className="stroke-black stroke-2"
-          d={`M ${5+10+110*i} 170 ${5-10+110*(i+1)} 170`}
+          d={`M ${5+10+110*i} 220 ${5-10+110*(i+1)} 220`}
         />
 
         {/* races */}
         {Array.from(Array(Math.floor(a.length/3)).keys()).map(b => <>
           <rect
             x={5+12+110*i+31*0}
-            y={160-24-b*34}
+            y={210-24-b*34}
             className={`w-[24px] h-[24px] ${distStyles[i]}`}
           />
           <rect
             x={5+12+110*i+31*1}
-            y={160-24-b*34}
+            y={210-24-b*34}
             className={`w-[24px] h-[24px] ${distStyles[i]}`}
           />
           <rect
             x={5+12+110*i+31*2}
-            y={160-24-b*34}
+            y={210-24-b*34}
             className={`w-[24px] h-[24px] ${distStyles[i]}`}
           />
         </>)}
         {a.length%3 === 2 ? <>
           <rect
             x={5+12+110*i+15}
-            y={160-24-(Math.floor(a.length/3))*34}
+            y={210-24-(Math.floor(a.length/3))*34}
             className={`w-[24px] h-[24px] ${distStyles[i]}`}
           />
           <rect
             x={5+12+110*i+46}
-            y={160-24-(Math.floor(a.length/3))*34}
+            y={210-24-(Math.floor(a.length/3))*34}
             className={`w-[24px] h-[24px] ${distStyles[i]}`}
           />
         </> : a.length%3 === 1 ? <>
           <rect
             x={5+12+110*i+31*1}
-            y={160-24-(Math.floor(a.length/3))*34}
+            y={210-24-(Math.floor(a.length/3))*34}
             className={`w-[24px] h-[24px] ${distStyles[i]}`}
           />
         </> : null}
@@ -93,7 +94,7 @@ export default function GovernorDistributionPreview({ incumbents, averagedPolls 
         {selectedBin===i && <>
           <text
             x={5+10+110*i+Math.floor((85-10*distLabels[i].length)/2)}
-            y={200}
+            y={250}
             className={`text-lg font-medium uppercase ${i>=5 ? 'fill-red-500' : i<=3 ? 'fill-blue-500' : 'fill-neutral-500'}`}
           >
             {distLabels[i]}
@@ -105,7 +106,7 @@ export default function GovernorDistributionPreview({ incumbents, averagedPolls 
           x={5+110*i}
           y={0}
           width={110}
-          height={170}
+          height={220}
           className={`fill-transparent`}
           onMouseEnter={() => setSelectedBin(i)}
           onMouseLeave={() => setSelectedBin(null)}
